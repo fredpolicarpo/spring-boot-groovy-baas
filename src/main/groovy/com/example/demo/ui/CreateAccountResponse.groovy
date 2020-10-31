e46bbd446ad30641dc6f23021ab429608d00c5f4
Fred <frederico.martins@ebanx.com>
2020-10-31 15:34:42 -0300

    Handling http status code on controller

--- src/main/groovy/com/example/demo/ui/CreateAccountResponse.groovy
+++ src/main/groovy/com/example/demo/ui/CreateAccountResponse.groovy
@@ -1,13 +1,17 @@
 package com.example.demo.ui
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 import groovy.transform.Canonical
 
 @Canonical
+@JsonInclude(JsonInclude.Include.NON_NULL)
 class CreateAccountResponse {
     @JsonProperty("document_number")
     String documentNumber
 
     @JsonProperty("created_at")
     String createdAt
+
+    String error
 }
