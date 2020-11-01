0fc3b3fda339969a22c0893980fe7b744dfc1088
Fred <frederico.martins@ebanx.com>
2020-11-01 18:40:25 -0300

    Add operation to create transaction on the Interactor

--- src/main/groovy/com/fredpolicarpo/baas/business/Interactor.groovy
+++ src/main/groovy/com/fredpolicarpo/baas/business/Interactor.groovy
@@ -1,11 +1,17 @@
 package com.fredpolicarpo.baas.business
 
 import com.fredpolicarpo.baas.business.entities.Account
+import com.fredpolicarpo.baas.business.entities.OperationType
+import com.fredpolicarpo.baas.business.entities.Transaction
 import com.fredpolicarpo.baas.business.exceptions.AccountNotFoundException
 import com.fredpolicarpo.baas.business.ports.AccountRepository
 import com.fredpolicarpo.baas.business.exceptions.DuplicatedAccountNumberException
+import com.fredpolicarpo.baas.business.ports.TransactionRepository
+import com.fredpolicarpo.baas.business.services.BankTransaction
 import com.fredpolicarpo.baas.ui.CreateAccountRequest
 import com.fredpolicarpo.baas.ui.CreateAccountResponse
+import com.fredpolicarpo.baas.ui.CreateTransactionRequest
+import com.fredpolicarpo.baas.ui.CreateTransactionResponse
 import com.fredpolicarpo.baas.ui.GetAccountResponse
 
 import static com.fredpolicarpo.baas.ui.CreateAccountRequestValidator.validate
@@ -17,6 +23,8 @@ import groovy.transform.Canonical
 @Canonical
 class Interactor {
     final AccountRepository accountRepository
+    final TransactionRepository transactionRepository
+    final BankTransaction bankTransaction
     final Timer timer
 
     CreateAccountResponse createAccount(CreateAccountRequest createAccountRequest) {
@@ -41,5 +49,23 @@ class Interactor {
         }
 
         return new GetAccountResponse(documentNumber: optionalAccount.get().documentNumber, accountId: optionalAccount.get().id)
+    }
+
+    CreateTransactionResponse createTransaction(CreateTransactionRequest request) {
+        final Long accountId = Long.valueOf(request.accountId)
+        final Optional<Account> optionalAccount = accountRepository.findById(accountId)
+
+        if (optionalAccount.isEmpty()) {
+            throw new AccountNotFoundException(accountId)
+        }
+
+        final Transaction transaction = bankTransaction.execute(
+                OperationType.fromCode(Integer.valueOf(request.operationTypeId)),
+                new BigDecimal(request.amount), optionalAccount.get()
+        )
+
+        final Transaction savedTransaction = transactionRepository.save(transaction)
+
+        return new CreateTransactionResponse(String.valueOf(savedTransaction.id), savedTransaction.eventDate.toString())
     }
 }
